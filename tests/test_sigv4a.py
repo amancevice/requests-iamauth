8dd578f246c402d66960a4cab1340948ffe3a756
Layla Franke <layla.franke@enit.io>
2025-04-14 17:21:17 +0200

    support multi-value query params

--- tests/test_sigv4a.py
+++ tests/test_sigv4a.py
@@ -1,3 +1,5 @@
+from urllib.parse import urlparse, parse_qsl
+
 import requests
 from botocore.compat import awscrt
 
@@ -23,3 +25,18 @@ class TestIAMAuth:
         prq = req.prepare()
         assert "X-Amz-Date" in prq.headers
         assert "Authorization" in prq.headers
+
+    def test_call_with_query_params(self):
+        req = requests.Request(
+            "GET",
+            "http://example.com/",
+            auth=self.subject,
+            params={"singleValueParam": "val1", "multiValueParam": ["val2", "val3"]},
+        )
+        prq = req.prepare()
+        query = urlparse(prq.url).query
+        assert parse_qsl(query) == [
+            ("singleValueParam", "val1"),
+            ("multiValueParam", "val2"),
+            ("multiValueParam", "val3"),
+        ]
